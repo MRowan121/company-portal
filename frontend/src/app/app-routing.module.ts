6fc37c5e4cc9455b7b26a1caf2acd0a5737faf39
Matt Rowan <matt.rowan121@gmail.com>
2023-09-25 15:08:26 -0400

    Add routes to various components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { UserRegistryComponent } from './user-registry/user-registry.component';
+import { TeamsComponent } from './teams/teams.component';
+import { CompanyComponent } from './company/company.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'select-company', component: CompanyComponent },
+  { path: 'announcements', component: AnnouncementsComponent },
+  { path: 'teams', component: TeamsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'user-registry', component: UserRegistryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
