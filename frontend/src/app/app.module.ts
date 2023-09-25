2fe525c16dc38b6c7c425cd8a551ed4d870fc4e7
Matt Rowan <matt.rowan121@gmail.com>
2023-09-25 14:58:46 -0400

    Create teams component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
 import { AnnouncementsComponent } from './announcements/announcements.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     LoginComponent,
-    AnnouncementsComponent
+    AnnouncementsComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
