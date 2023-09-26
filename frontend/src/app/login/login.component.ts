221f2922578448df2b5d3203f744f94b30e83c53
mbbrill <mbbrill@yahoo.com>
2023-09-25 17:52:44 -0700

    stylized login screen, created username and password gets and sets

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = ""
+  password: string = ""
 
+  //want to take username given from input on login page and parse through database to see if user exists
+  getUsername(value: string){
+    this.username = value;
+    localStorage.setItem("user", this.username)
+    console.log(this.username)
+  }
+
+  getPassword(value:string){
+    this.password = value;
+    localStorage.setItem("password", this.password)
+    console.log(this.password)
+  }
 }
+
+
+
+
