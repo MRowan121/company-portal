18b746c053f9fb862161a0f96b8e50dc9760f867
Matt Rowan <matt.rowan121@gmail.com>
2023-09-27 12:25:06 -0400

    Add html rendering for team card

--- frontend/src/app/projects/projects.component.ts
+++ frontend/src/app/projects/projects.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects: any[] = [];
+  constructor() {}
 
+  ngOnInit() {
+    this.getProjects();
+  }
+
+  async getProjects() {
+    const companyId = localStorage.getItem('selectedCompanyId');
+    // Hard coded this for testing. Need functionality to get id of selected team.
+    const teamId = 1;
+    console.log(companyId);
+    const request = await axios.get(
+      `http://localhost:8080/company/${companyId}/teams/${teamId}/projects`
+    );
+    this.projects = request.data;
+    console.log(this.projects);
+  }
 }
--- frontend/src/app/teams/teams.component.ts
+++ frontend/src/app/teams/teams.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import axios from 'axios';
 
+interface Team {
+  teamId: number;
+  name: string;
+  projects: any[];
+  memberProfiles: string[];
+  memberNames: string[];
+}
+
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
   styleUrls: ['./teams.component.css'],
 })
 export class TeamsComponent implements OnInit {
-  teams: any[] = [];
+  teams: Team[] = [];
 
   constructor() {}
   ngOnInit() {
@@ -16,11 +24,39 @@ export class TeamsComponent implements OnInit {
 
   async getTeams() {
     const companyId = localStorage.getItem('selectedCompanyId');
-    console.log(companyId);
     const request = await axios.get(
       `http://localhost:8080/company/${companyId}/teams`
     );
-    this.teams = request.data;
+    this.teams = request.data.map((obj: any) => {
+      return {
+        teamId: obj.id,
+        name: obj.name,
+        projects: [],
+        memberProfiles: obj.users,
+        memberNames: [],
+      };
+    });
+    this.getMemberNames();
+    this.getProjects(companyId);
     console.log(this.teams);
+  }
+
+  async getProjects(companyId: any) {
+    this.teams.forEach(async (team: Team) => {
+      const request = await axios.get(
+        `http://localhost:8080/company/${companyId}/teams/${team.teamId}/projects`
+      );
+      team.projects = request.data;
+    });
+  }
+
+  getMemberNames() {
+    this.teams.forEach((team: Team) =>
+      team.memberProfiles.forEach((member: any) => {
+        team.memberNames.push(
+          `${member.profile.firstname} ${member.profile.lastname[0]}.`
+        );
+      })
+    );
   }
 }
