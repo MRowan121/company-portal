167527f25d92f2ec78f3d94da1cfe134784ac6c8
Matt Rowan <matt.rowan121@gmail.com>
2023-09-27 10:52:57 -0400

    Fetch teams and save to variable

--- frontend/src/app/teams/teams.component.ts
+++ frontend/src/app/teams/teams.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-teams',
   templateUrl: './teams.component.html',
-  styleUrls: ['./teams.component.css']
+  styleUrls: ['./teams.component.css'],
 })
-export class TeamsComponent {
+export class TeamsComponent implements OnInit {
+  teams: any[] = [];
 
+  constructor() {}
+  ngOnInit() {
+    this.getTeams();
+  }
+
+  async getTeams() {
+    const companyId = localStorage.getItem('selectedCompanyId');
+    console.log(companyId);
+    const request = await axios.get(
+      `http://localhost:8080/company/${companyId}/teams`
+    );
+    this.teams = request.data;
+    console.log(this.teams);
+  }
 }
