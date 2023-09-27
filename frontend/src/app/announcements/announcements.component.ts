671796b395d7f2e051c42eec4b5b43e997ac9c13
Matt Rowan <matt.rowan121@gmail.com>
2023-09-27 10:18:34 -0400

    Update announcement functionality

--- frontend/src/app/announcements/announcements.component.ts
+++ frontend/src/app/announcements/announcements.component.ts
@@ -1,27 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-const axios = require('axios')
+import axios from 'axios';
+
+interface ProfileDTO {
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+}
+
+interface BasicUserDTO {
+  id: number;
+  profile: ProfileDTO;
+  isAdmin: boolean;
+  active: boolean;
+  status: string;
+}
+
+interface AnnouncementDTO {
+  id: number;
+  date: string;
+  title: string;
+  message: string;
+  author: BasicUserDTO;
+}
+
+interface Announcement {
+  firstName: string;
+  lastName: string;
+  message: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-announcements',
   templateUrl: './announcements.component.html',
   styleUrls: ['./announcements.component.css'],
 })
-export class AnnouncementsComponent implements OnInit{
-  announcements: any[] = [];
-  companyId: string | null = ""
-  constructor(){}
-    ngOnInit() {
-      // console.log(this.companyId);
-      this.getAnnouncements();
-    }
-    
-    async getAnnouncements(){
-      this.companyId = localStorage.getItem('selectedCompanyId')
-      // const request = await axios.get("http://localhost:8080/company/"+this.companyId+"/announcements")
-      const request = await axios.get(`http://localhost:8080/company/${this.companyId}/announcements`)
-      this.announcements = request.data
-      console.log(request.data);
-      console.log(this.announcements)
-    }
-}
+export class AnnouncementsComponent implements OnInit {
+  companyId: string | null = '';
+  announcements: Announcement[] = [];
 
+  constructor() {}
+
+  ngOnInit() {
+    this.getAnnouncements();
+  }
+
+  async getAnnouncements() {
+    this.companyId = localStorage.getItem('selectedCompanyId');
+    const request = await axios.get(
+      `http://localhost:8080/company/${this.companyId}/announcements`
+    );
+    this.announcements = request.data.map((obj: AnnouncementDTO) => {
+      return {
+        firstName: obj.author.profile.firstname,
+        lastName: obj.author.profile.lastname,
+        message: obj.message,
+        date: obj.date,
+      };
+    });
+  }
+}
--- frontend/src/app/company/company.component.ts
+++ frontend/src/app/company/company.component.ts
@@ -1,58 +1,58 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
-const axios = require('axios')
+import { Router } from '@angular/router';
+const axios = require('axios');
 
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
-  styleUrls: ['./company.component.css']
+  styleUrls: ['./company.component.css'],
 })
 export class CompanyComponent implements OnInit {
   isAdmin: boolean = false;
   isLoggedIn: boolean = false;
-  companyNames: string[] = []
-  user: any ={}
-  selectedCompanyId: number = 0
+  companyNames: string[] = [];
+  user: any = {};
+  selectedCompanyId: number = 0;
 
-  constructor(private router: Router){
+  constructor(private router: Router) {}
 
-  }
-
-  ngOnInit(){
-    
-    const savedUser = localStorage.getItem("user")
-    if(savedUser){
-      this.user = JSON.parse(savedUser)
+  ngOnInit() {
+    const savedUser = localStorage.getItem('user');
+    if (savedUser) {
+      this.user = JSON.parse(savedUser);
     }
-    console.log(this.user)
-    this.getCompanies()
-    this.isAdmin = localStorage.getItem('isAdmin') === 'true' ? true:false
-    this.isLoggedIn = localStorage.getItem('isLoggedIn') === 'true' ? true:false
-    if(this.isLoggedIn){
-      if(!this.isAdmin){
-        this.router.navigate(['/announcements'])
+    console.log(this.user);
+    this.getCompanies();
+    this.isAdmin = localStorage.getItem('isAdmin') === 'true' ? true : false;
+    this.isLoggedIn =
+      localStorage.getItem('isLoggedIn') === 'true' ? true : false;
+    if (this.isLoggedIn) {
+      if (!this.isAdmin) {
+        this.router.navigate(['/announcements']);
       }
-    }else{
-      this.router.navigate(['/'])
+    } else {
+      this.router.navigate(['/']);
     }
   }
 
-  async getCompanies(){
-    const request = await axios.get("http://localhost:8080/company")
-    this.companyNames = request.data
-    // console.log(this.companyNames)
-    console.log(request)
+  async getCompanies() {
+    const request = await axios.get('http://localhost:8080/company');
+    this.companyNames = request.data;
   }
 
-  getCompanyId(){
-    localStorage.getItem('user')
+  getCompanyId() {
+    localStorage.getItem('user');
   }
 
-  chooseCompany(value:string){
-    const selectedCompany = this.user.companies.filter((company: any) => company.name === value)
-    this.selectedCompanyId = selectedCompany[0].id
-    localStorage.setItem('selectedCompanyId', this.selectedCompanyId.toString())
-    this.router.navigate(['/announcements'])
+  chooseCompany(value: string) {
+    const selectedCompany = this.user.companies.filter(
+      (company: any) => company.name === value
+    );
+    this.selectedCompanyId = selectedCompany[0].id;
+    localStorage.setItem(
+      'selectedCompanyId',
+      this.selectedCompanyId.toString()
+    );
+    this.router.navigate(['/announcements']);
   }
-
 }
