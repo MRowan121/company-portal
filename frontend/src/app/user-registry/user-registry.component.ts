f2cdffdbb5a1920751c3e8c7400639d1fc780239
Matt Rowan <matt.rowan121@gmail.com>
2023-09-27 15:27:01 -0400

    Fetch and display company users

--- frontend/src/app/user-registry/user-registry.component.ts
+++ frontend/src/app/user-registry/user-registry.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import axios from 'axios';
+
+interface User {
+  name: string;
+  email: string;
+  active: boolean;
+  admin: boolean;
+  status: string;
+}
 
 @Component({
   selector: 'app-user-registry',
   templateUrl: './user-registry.component.html',
-  styleUrls: ['./user-registry.component.css']
+  styleUrls: ['./user-registry.component.css'],
 })
-export class UserRegistryComponent {
+export class UserRegistryComponent implements OnInit {
+  users: User[] = [];
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.getCompanyUsers();
+  }
 
+  async getCompanyUsers() {
+    const companyId = localStorage.getItem('selectedCompanyId');
+    const request = await axios.get(
+      `http://localhost:8080/company/${companyId}/users`
+    );
+    this.users = request.data.map((obj: any) => {
+      return {
+        name: `${obj.profile.firstname} ${obj.profile.lastName}`,
+        email: obj.profile.email,
+        active: obj.active,
+        admin: obj.isAdmin,
+        status: obj.status,
+      };
+    });
+  }
 }
