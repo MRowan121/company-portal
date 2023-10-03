6265bb99fefd52b5b20030671c2b5b2f6c1bf2e8
Matt Rowan <matt.rowan121@gmail.com>
2023-10-03 09:19:47 -0400

    Add get user by id  request

--- backend/src/main/java/com/cooksys/groupfinal/controllers/UserController.java
+++ backend/src/main/java/com/cooksys/groupfinal/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.cooksys.groupfinal.controllers;
 
+import com.cooksys.groupfinal.dtos.BasicUserDto;
 import com.cooksys.groupfinal.dtos.CredentialsDto;
 import com.cooksys.groupfinal.dtos.FullUserDto;
 import com.cooksys.groupfinal.services.UserService;
@@ -18,6 +19,11 @@ public class UserController {
     @CrossOrigin(origins = "*")
     public FullUserDto login(@RequestBody CredentialsDto credentialsDto) {
         return userService.login(credentialsDto);
+    }
+
+    @GetMapping("/{id}")
+    public BasicUserDto getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
     }
 
 }
--- backend/src/main/java/com/cooksys/groupfinal/services/impl/UserServiceImpl.java
+++ backend/src/main/java/com/cooksys/groupfinal/services/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cooksys.groupfinal.services.impl;
 
+import com.cooksys.groupfinal.dtos.BasicUserDto;
 import com.cooksys.groupfinal.dtos.CredentialsDto;
 import com.cooksys.groupfinal.dtos.FullUserDto;
 import com.cooksys.groupfinal.entities.Credentials;
@@ -7,6 +8,7 @@ import com.cooksys.groupfinal.entities.User;
 import com.cooksys.groupfinal.exceptions.BadRequestException;
 import com.cooksys.groupfinal.exceptions.NotAuthorizedException;
 import com.cooksys.groupfinal.exceptions.NotFoundException;
+import com.cooksys.groupfinal.mappers.BasicUserMapper;
 import com.cooksys.groupfinal.mappers.CredentialsMapper;
 import com.cooksys.groupfinal.mappers.FullUserMapper;
 import com.cooksys.groupfinal.repositories.UserRepository;
@@ -23,8 +25,9 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final FullUserMapper fullUserMapper;
     private final CredentialsMapper credentialsMapper;
+    private final BasicUserMapper basicUserMapper;
 
-    private User findUser(String username) {
+    private User findUserByUsername(String username) {
         Optional<User> user = userRepository.findByCredentialsUsernameAndActiveTrue(username);
         if (user.isEmpty()) {
             throw new NotFoundException("The username provided does not belong to an active user.");
@@ -39,7 +42,7 @@ public class UserServiceImpl implements UserService {
             throw new BadRequestException("A username and password are required.");
         }
         Credentials credentialsToValidate = credentialsMapper.dtoToEntity(credentialsDto);
-        User userToValidate = findUser(credentialsDto.getUsername());
+        User userToValidate = findUserByUsername(credentialsDto.getUsername());
         if (!userToValidate.getCredentials().equals(credentialsToValidate)) {
             throw new NotAuthorizedException("The provided credentials are invalid.");
         }
@@ -48,6 +51,20 @@ public class UserServiceImpl implements UserService {
             userRepository.saveAndFlush(userToValidate);
         }
         return fullUserMapper.entityToFullUserDto(userToValidate);
+    }
+
+    @Override
+    public User getUser(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new NotFoundException("No user with id: " + id);
+        }
+        return optionalUser.get();
+    }
+
+    @Override
+    public BasicUserDto getUserById(Long id) {
+        return basicUserMapper.entityToBasicUserDto(getUser(id));
     }
 
 
